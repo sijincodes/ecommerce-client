68f94d7a2b5824a5e965cb6763289dc0301db1b1
sijincodes <sijinmathews03@gmail.com>
2023-01-19 16:46:05 +0100

    grid pics done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Categories from '../../components/Categories/Categories'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from '../../components/Slider/Slider'
 
@@ -9,6 +10,7 @@ function Home() {
     <div className='home'>
       <Slider />
       <FeaturedProducts  type="featured"/>
+      <Categories />
       <FeaturedProducts  type="trending"/>
     </div>
   )
