f64927634d08ca52f8e13bde1b5687deedcbcfeb
sijincodes <sijinmathews03@gmail.com>
2023-01-19 12:40:55 +0100

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+
+import "./Footer.scss";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Perspiciatis, praesentium? Saepe hic sequi numquam reprehenderit
+            nobis ex accusantium dolorem, eum impedit pariatur rem soluta, fuga,
+            tempore aliquam debitis blanditiis? Ducimus.
+          </span>
+        </div>
+        <div className="item">
+        <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Perspiciatis, praesentium? Saepe hic sequi numquam reprehenderit
+            nobis ex accusantium dolorem, eum impedit pariatur rem soluta, fuga,
+            tempore aliquam debitis blanditiis? Ducimus.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span  className="logo">My Shop</span>
+          <span className="copyright"> © Copyright 2023. All Rights Reserved</span>
+        </div>
+        <div className="right">
+          <img src="./../../images/payment.png" alt="payment-img" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
